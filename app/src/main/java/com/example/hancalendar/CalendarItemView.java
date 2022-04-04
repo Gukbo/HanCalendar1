4aa26496b1a53e3ab14aed7ce99be285e1b27d1a
Gukbo <39515692+Gukbo@users.noreply.github.com>
2022-04-04 16:48:04 +0900

    CalendarItemView.java 추가

---
+++ app/src/main/java/com/example/hancalendar/CalendarItemView.java
@@ -0,0 +1,49 @@
+package com.example.hancalendar;
+
+import static android.graphics.Color.WHITE;
+import android.content.Context;
+import android.graphics.Color;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+// 본문에는 import android.support.v7.app.AppCompatActivity; 였는데 오류가 생겨서 대체했습니다.
+// 찾아보니 아티팩트의 버전이 달라서 생긴 문제였습니다.
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.webkit.WebHistoryItem;
+
+public class CalendarItemView extends AppCompatTextView{
+
+    private CalendarItem item;
+
+    public CalendarItemView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CalendarItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() { // 바탕의 색을 하얗게 초기화시키는 내용입니다.
+        setBackgroundColor(Color.WHITE);
+    }
+
+    public CalendarItem getItem() {
+        return item;
+    }
+
+    public void setItem(CalendarItem item) {
+        this.item = item;
+
+        int day = item.getDay(); // 받아온 날짜가 0이 아니라면 색도 입히고 중앙정렬도 하고 폰트의 크기도 지정합니다.
+        if (day != 0) {
+            setText(String.valueOf(day));
+            setGravity(Gravity.CENTER);
+            setTextColor(Color.BLACK);
+            setTextSize(20);
+        } else { // 받아온 날짜가 0이면 TEXT를 빈칸으로 둡니다.
+            setText("");
+        }
+    }
+}
